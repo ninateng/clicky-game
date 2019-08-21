e7ec3b16cf2ddec954b60c2e0e9e8d1ea48e7410
ninateng <niinateng@gmail.com>
2019-08-21 12:25:32 -0700

    Updates

---
+++ precache-manifest.958f6ccb08dbfc0d4d63da689f4993fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bbea92c61ad87b7d5241250c6b7aae5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb23027bdc890ce5006a",
+    "url": "/static/css/main.2d7d5ee8.chunk.css"
+  },
+  {
+    "revision": "22b162a17c2685a38d97",
+    "url": "/static/js/2.100e50e4.chunk.js"
+  },
+  {
+    "revision": "fb23027bdc890ce5006a",
+    "url": "/static/js/main.f5520b1c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
